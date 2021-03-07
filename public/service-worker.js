20733a61e64f7808ea6a4a98f613e66412c5e814
Nick Hill <nickhill2380@gmail.com>
2021-03-07 18:04:55 -0500

    coming back online bring offline transactions with it

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,8 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
   "./index.html",
-  ".public/css/styles.css"
+  "./public/css/styles.css",
+  "./public/js/index.js"
 ];
 
 // Respond with cached resources
