86b25f5d486daa2fb313edbc4ce2e7e050f49ba6
Nick Hill <nickhill2380@gmail.com>
2021-03-09 17:13:55 -0500

    fixed service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,8 @@ const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
   "./index.html",
   "./public/css/styles.css",
-  "./public/js/index.js"
+  "./public/js/index.js",
+  "./public/js/ids.js"
 ];
 
 // Respond with cached resources
@@ -19,9 +20,6 @@ self.addEventListener('fetch', function (e) {
         console.log('file is not cached, fetching : ' + e.request.url)
         return fetch(e.request)
       }
-
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
     })
   )
 })
